a6c0a24b34ab211998d8ed38cc41f2d0777d7ba1
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-01-04 09:29:48 +0200

    product repo test commit

--- delivery-backend/src/main/java/com/nnmpizza/delivery/repository/ProductRepository.java
+++ delivery-backend/src/main/java/com/nnmpizza/delivery/repository/ProductRepository.java
@@ -4,10 +4,13 @@ import com.nnmpizza.delivery.models.Member;
 import com.nnmpizza.delivery.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Member> findByName(String name);
 
     Boolean existsByName(String name);
+
+    List<Product> findAll();
 }
