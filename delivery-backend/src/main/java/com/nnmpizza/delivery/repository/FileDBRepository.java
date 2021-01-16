9442edb7a02c03d23b0a3f8b41466b03bd3a6c94
NaidDzhan Durmush <nc.durmush@gmail.com>
2021-01-17 00:40:20 +0200

    feat: file uploading

---
+++ delivery-backend/src/main/java/com/nnmpizza/delivery/repository/FileDBRepository.java
@@ -0,0 +1,9 @@
+package com.nnmpizza.delivery.repository;
+
+import com.nnmpizza.delivery.models.FileDB;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
+}
